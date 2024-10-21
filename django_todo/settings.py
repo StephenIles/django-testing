24a492c1b7b9063a4348a056cf1c58cf277e2c15
Stephen Iles <stephenciles@gmail.com>
2024-10-21 08:23:53 +0100

    Fix typo

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.eviron.get('SECRET_KEY', 'django-insecure-7)ahe=@1p_pq_8yxsmp@z=$(55$th9i44!8zk*c=0bul1z9^7(')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7)ahe=@1p_pq_8yxsmp@z=$(55$th9i44!8zk*c=0bul1z9^7(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
